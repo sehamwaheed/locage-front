bef5fb532dc73d6b751afee9372a06bb7e001689
dina waheed <dinawaheed3@gmail.com>
2021-05-19 14:14:06 +0200

    fontawsom

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Navbar/navbar/navbar.component';
+import { SearchComponent } from './Navbar/Navbar-Component/search/search.component';
+import { SubNavbarComponent } from './Navbar/Navbar-Component/sub-navbar/sub-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    SubNavbarComponent
   ],
   imports: [
     BrowserModule,
